7293a449ca41228810e8304de11304980fedbab5
Palesa-1 <159442666+Palesa-1@users.noreply.github.com>
2025-09-08 14:32:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 
-let submitButton = document.getElementById('submit-button');
 
+function showname(response) {
+  new Typewriter("#nameOutput", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
-submitButton.addEventListener('click', function() {
-    alert('Thank you for your submission!');
-});
+function generator(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#name-generator");
+  let apikey = "ta654bee04c3753o585ff2189fe74b0a";
+  let context = "You are a creative name generator. You generate unique and catchy names based on user instructions. list three names in a list format. using the <strong> tag for the names. ue the <li> tag for the list. Do not use the <ul> tag. the names are either for boys or girls.";
+  let prompt = `User instructions: Generate name for ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+
+  let outcome = document.querySelector("#nameOutput");
+  outcome.classList.remove("hidden"); 
+  outcome.innerHTML = `<div class="generating">⏳ Generating names </div>`;
+
+  axios.get(apiURL).then(showname);
+}
+
+let form = document.querySelector("#name-generator"); // Make sure your HTML has a form with this ID
+form.addEventListener("submit", generator);
